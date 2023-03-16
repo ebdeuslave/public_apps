6b0f1072e377bd792684bb707cb4038e324c986a
ebdeuslave <63257740+ebdeuslave@users.noreply.github.com>
2023-03-16 23:06:02 +0100

    Create ai_pic_generator.py

---
+++ ai_pic_generator.py
@@ -0,0 +1,34 @@
+# Model
+from tensorflow import keras
+import keras_cv
+keras.mixed_precision.set_global_policy("mixed_float16")
+
+# Visualization
+import matplotlib.pyplot as plt
+
+# Save the image
+from PIL import Image
+
+# Create a model
+model = keras_cv.models.StableDiffusion(img_height=512, 
+                                        img_width=512,
+                                        jit_compile=True)
+def plot_images(images):
+    # Set figure size
+    plt.figure(figsize=(20, 20))
+    # Loop through each image
+    for i in range(len(images)):
+        # Subplot setup
+        ax = plt.subplot(1, len(images), i + 1)
+        # Plot each image
+        plt.imshow(images[i])
+        # Do not show axis
+        plt.axis("off")
+
+
+# Create images from text
+images = model.text_to_image(prompt="Max in his cozy little hole in the wall",
+                             batch_size=2)
+
+# Plot the images
+plot_images(images)
